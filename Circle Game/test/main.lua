83a5cb8367b13fcf472b2e4914aad3207bb27c55
satansgimpsuit <satansgimpsuit@gmail.com>
2023-02-10 05:43:12 +0000

    changed layout of game info

--- Circle Game/test/main.lua
+++ Circle Game/test/main.lua
@@ -30,23 +30,23 @@ function love.mousemoved(_x, _y, dx, dy)
     end
 end
 function barColor(time)
-    love.graphics.setColor(time/15, 0.0, 1.0 - time/15)
+    love.graphics.setColor(time/timer, 0.0, 1.0 - time/timer)
 end
 function love.update(dt)
     target = (circleX - randX)^2 + (circleY - randY)^2
     if target < 10 then
-        randX = love.math.random(radius, windowWidth - radius + 10)
-        randY = love.math.random(radius, windowHeight - radius + 10)
+        randX = love.math.random(radius, windowWidth - radius + 1)
+        randY = love.math.random(radius, windowHeight - radius + 1)
         count = count + 1
     end
     
     countdown = countdown + dt
     if countdown > timer then
         if count >= 5 then
-            timer = timer - round
+            timer = timer - 2
             count = 0
             round = round + 1
-        end
+        else 
         countdown = 0
     end
     
@@ -61,8 +61,9 @@ function love.draw()
     love.graphics.setColor(0.7, 0.1, 0.4)
     love.graphics.circle('line', randX, randY, radius + 10, 100)
     love.graphics.setColor(0.5, 0.5, 0.5)
-    love.graphics.print(tostring(count), windowWidth/2, 10)
-    love.graphics.print(tostring(countdown), windowWidth/2, 5)
+    love.graphics.print(tostring(count), windowWidth/3, 10)
+    love.graphics.print(tostring(countdown), windowWidth/2, 0)
+    love.graphics.print(tostring(round), windowWidth * 2/3, )
     barColor(countdown)
-    love.graphics.rectangle('fill', countdown * windowWidth / 30, 0, windowWidth - countdown * windowWidth / 15, 10)
+    love.graphics.rectangle('fill', countdown * windowWidth / 2*timer, 0, windowWidth - countdown * windowWidth / timer, 10)
 end
